b481f87dab07782b4a21e033d5d64b121e423703
Huang Bo <huangbop@gmail.com>
2015-11-24 15:31:42 +0800

    Any sum

--- knapsack/main.c
+++ knapsack/main.c
@@ -1,83 +1,49 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
-/* http://www.geeksforgeeks.org/dynamic-programming-set-10-0-1-knapsack-problem/ */
 
-// A Dynamic Programming based solution for 0-1 Knapsack problem
-#include<stdio.h>
- 
-// A utility function that returns maximum of two integers
-int max(int a, int b) { return (a > b)? a : b; }
- 
-// Returns the maximum value that can be put in a knapsack of capacity W
-int knapSack(int W, int wt[], int val[], int n)
+unsigned char *flag;
+int length;
+
+void any_sum(int n, int m)
 {
-    int i, w;
-    int K[n+1][W+1];
- 
-    // Build table K[][] in bottom up manner
-    for (i = 0; i <= n; i++)
-    {
-        for (w = 0; w <= W; w++)
-        {
-            if (i==0 || w==0)
-                K[i][w] = 0;
-            else if (wt[i-1] <= w)
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
-            else
-                K[i][w] = K[i-1][w];
+    int i;
+
+    if (n == 0 || m == 0)
+        return;
+
+    if (m < n)
+        n = m;
+
+    if (m == n) {
+        flag[n-1] = 1;
+
+        for (i = 0; i < length; i++) {
+            if (flag[i])
+                printf("%d ", i + 1);
         }
+        printf("\n");
     }
- 
-    return K[n][W];
+
+    flag[n-1] = 1;              /* Select nth */
+    any_sum(n - 1, m - n);
+    flag[n-1] = 0;              /* Not select nth */
+    any_sum(n - 1, m);
 }
- 
-int main()
+
+int main(void)
 {
-    int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("%d", knapSack(W, wt, val, n));
+    int n, m;
+
+    printf("Input n and m:\n");
+    scanf("%d%d", &n, &m);
+
+    flag = (unsigned char *)malloc(n);
+    memset(flag, 0, n);
+    length = n;
+
+    any_sum(n, m);
+
     return 0;
 }
-
-
-#if 0
-/* A Naive recursive implementation of 0-1 Knapsack problem */
-#include<stdio.h>
- 
-// A utility function that returns maximum of two integers
-int max(int a, int b) { return (a > b)? a : b; }
- 
-// Returns the maximum value that can be put in a knapsack of capacity W
-int knapSack(int W, int wt[], int val[], int n)
-{
-   // Base Case
-   if (n == 0 || W == 0)
-       return 0;
- 
-   // If weight of the nth item is more than Knapsack capacity W, then
-   // this item cannot be included in the optimal solution
-   if (wt[n-1] > W)
-       return knapSack(W, wt, val, n-1);
- 
-   // Return the maximum of two cases: 
-   // (1) nth item included 
-   // (2) not included
-   else return max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),
-                    knapSack(W, wt, val, n-1)
-                  );
-}
- 
-// Driver program to test above function
-int main()
-{
-    int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("%d", knapSack(W, wt, val, n));
-    return 0;
-}
-#endif
-
-
